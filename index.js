c1a0aa12f3d75fa2e9cadcd66988e1724559720b
skololaviron <skolola@vironit.ru>
2019-01-02 14:49:04 +0300

    fix uptime issue and better placeholder
    bette markup
    
    bette markup
    
    add antisleep
    
    add express to listening ports
    
    add express to listening ports
    
    fix problem fix axios

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
-
+const express = require("express");
+const axios = require("axios");
+const app = express();
+setInterval(function() {
+  axios.get("https://intense-ravine-75760.herokuapp.com/");
+}, 300000);
 bot.onText(/\/motto/, (msg) => {
   bot.sendMessage(msg.chat.id, "Вуд Дивижн Эстетичней Парижа");
 });
@@ -15,9 +20,9 @@ bot.onText(/\/helpMeButler/, (msg) => {
 });
 
 bot.onText(/\/startButler/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Welcome", {
+  bot.sendMessage(msg.chat.id, "How can I help you, sir?", {
     reply_markup: {
-      keyboard: [["/motto"], ["/whoislambert"], ["/help"]]
+      keyboard: [["/motto"], ["/whoislambert"], ["/helpMeButler"]]
     }
   });
 });
@@ -34,6 +39,6 @@ bot.on("message", (msg) => {
   }
 });
 
-module.exports = (req, res) => {
-  res.end(`Hi!`);
-};
+app.listen(process.env.PORT || 3000, () => {
+  console.log("listening");
+});
